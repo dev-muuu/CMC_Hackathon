95b8992890db3ef96a5eef750497402ae61af6bb
Ssooya <bsy24660@naver.com>
2022-09-24 17:08:18 +0900

    #feat: 투표 도메인 관련 파일 추가

---
+++ Hackathon/domain/Vote/CrownViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CrownViewController.swift
+//  Hackathon
+//
+//  Created by 박지윤 on 2022/09/24.
+//
+
+import UIKit
+
+class CrownViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
