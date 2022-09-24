28417b5e55d6e59b47478ec97ed6b362ac562819
Ssooya <bsy24660@naver.com>
2022-09-25 06:07:15 +0900

    #feat: api 파일 추가

---
+++ Hackathon/domain/Service/Comment/CommentDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CommentDataManager.swift
+//  Hackathon
+//
+//  Created by 박지윤 on 2022/09/25.
+//
+
+import Foundation
---
+++ Hackathon/domain/Service/Vote/VoteDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  VoteDataManager.swift
+//  Hackathon
+//
+//  Created by 박지윤 on 2022/09/25.
+//
+
+import Foundation
