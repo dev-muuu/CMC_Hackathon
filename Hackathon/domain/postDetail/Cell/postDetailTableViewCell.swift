a0b18783bdb71e24c6ead58aedd6f2a088474e6c
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2022-09-24 21:05:43 +0900

    Docs: Add files

---
+++ Hackathon/domain/postDetail/Cell/postDetailTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  postDetailTableViewCell.swift
+//  Hackathon
+//
+//  Created by 양채연 on 2022/09/24.
+//
+
+import Foundation
---
+++ Hackathon/domain/postDetail/postDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  postDetailViewController.swift
+//  Hackathon
+//
+//  Created by 양채연 on 2022/09/24.
+//
+
+import UIKit
+
+class postDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
